1c69a7963d40418d046643a5b20a658529f60c7c
linwei365 <linwei365@gmail.com>
2016-03-05 16:41:46 -0800

    dev branch

--- GoogleAdMobTest/ViewController.swift
+++ GoogleAdMobTest/ViewController.swift
@@ -7,16 +7,33 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        bannerView.adUnitID = "ca-app-pub-5085464511752371/9110553641"
+        bannerView.rootViewController = self
+        self.view.addSubview(bannerView)
+        let request = GADRequest()
+            request.testDevices = [""]
+        bannerView.loadRequest(request)
+        
+//        print("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
+//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+//        bannerView.rootViewController = self
+//        bannerView.loadRequest(GADRequest())
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
